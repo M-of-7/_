ee7f54654d62ff0dfaa3dc19f49428ee8a2e20a6
M-of-7 <fata_al7adriien@hotmail.com>
2025-10-23 15:05:56 +0300

    Add files via upload

--- supabase/functions/fetch-live-news/index.ts
+++ supabase/functions/fetch-live-news/index.ts
@@ -10,7 +10,7 @@ import { toByteArray } from 'npm:base64-js';
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'Authorization, X-Client-Info, apikey, Content-Type',
 };
 
 interface RSSFeed {
